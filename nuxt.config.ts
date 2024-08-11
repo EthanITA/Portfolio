d359bba75de822f99cee148cfc6413d59fb85023
madong <viaimperiale@gmail.com>
2024-08-11 16:29:45 +0200

    feat(nuxt.config): apply Nunito font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,4 +10,19 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: "en",
+      },
+      link: [
+        {
+          href: "https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap",
+          rel: "preload",
+          as: "style",
+          onload: "this.onload=null;this.rel='stylesheet'",
+        },
+      ],
+    },
+  },
 });
