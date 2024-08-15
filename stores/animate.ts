d11bb776da94fc8a3c7e2f3eeb664fa6925b97d7
madong <viaimperiale@gmail.com>
2024-08-15 23:31:21 +0200

    fix(stores/animate): add return

--- stores/animate.ts
+++ stores/animate.ts
@@ -2,4 +2,7 @@ import gsap from "gsap";
 
 export const useAnimate = defineStore("animate", () => {
   const instance = computed(() => (import.meta.client ? gsap : null));
+  return {
+    instance,
+  };
 });
