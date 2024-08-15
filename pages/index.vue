a606cf96bfeb9b97ccd87926f5730710fb9574bf
madong <viaimperiale@gmail.com>
2024-08-15 22:40:49 +0200

    fix(pages/index): more concise and precise text

--- pages/index.vue
+++ pages/index.vue
@@ -6,11 +6,11 @@
       <Wave class="items-center justify-center blur-3xl" />
     </div>
     <div class="hero-content text-gray-700 text-center">
-      <div class="max-w-lg space-y-8">
+      <div class="max-w-2xl space-y-8">
         <h1 class="text-7xl font-black">
-          <span>Hi. I'm Marco.</span>
+          <span>I'm Marco Dong.</span>
           <br />
-          <span>A Developer.</span>
+          <span class="text-primary">A Full-Stack Developer.</span>
         </h1>
         <p class="text-xl">
           I'm passionate about crafting web app and committed to deliver
