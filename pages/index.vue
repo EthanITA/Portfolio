e7744900d1f91e52bdd09f26cf084335151cb9d7
madong <viaimperiale@gmail.com>
2024-08-15 20:40:40 +0200

    fix(pages/index): spaces and sizes

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,18 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div class="hero h-[1024px] bg-primary-50">
-    <div class="hero-overlay overflow-hidden flex">
+  <div class="hero max-h-full h-[1024px]">
+    <div class="hero-overlay flex">
       <Wave class="items-center justify-center blur-3xl" />
     </div>
     <div class="hero-content text-gray-700 text-center">
-      <div class="max-w-lg space-y-4">
+      <div class="max-w-lg space-y-8">
         <h1 class="text-7xl font-black">
           <span>Hi. I'm Marco.</span>
           <br />
           <span>A Developer.</span>
         </h1>
-        <p class="text-lg">
+        <p class="text-xl">
           I'm passionate about crafting web app and committed to deliver
           high-quality work.
         </p>
