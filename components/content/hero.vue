e504f80b4441382b2c41a67973a36b35c400a6af
madong <viaimperiale@gmail.com>
2024-08-16 16:35:58 +0200

    refactor(pages/index): move into ContentHero.vue

---
+++ components/content/hero.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="hero max-h-full h-[1024px]">
+    <div class="hero-overlay flex">
+      <Wave class="items-center justify-center blur-3xl" />
+    </div>
+    <div class="hero-content text-gray-700 text-center">
+      <div class="max-w-2xl space-y-8">
+        <h1 class="text-7xl font-black">
+          <span>I'm Marco Dong.</span>
+          <br />
+          <span class="text-primary">A Full-Stack Developer.</span>
+        </h1>
+        <p class="text-xl">
+          I'm passionate about crafting web app and committed to deliver
+          high-quality work.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
