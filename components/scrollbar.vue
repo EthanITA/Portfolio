dcf75b361968a6063285426650cf12a3c96efa4c
madong <viaimperiale@gmail.com>
2024-08-17 19:09:53 +0200

    feat(components/scrollbar): extend hover area

--- components/scrollbar.vue
+++ components/scrollbar.vue
@@ -4,6 +4,8 @@ import type { WatchStopHandle } from "vue";
 const page = usePage();
 const cursor = useCursor();
 
+const hovering = ref(false);
+
 const unwatch = ref<WatchStopHandle>();
 
 const updateProgress = () =>
@@ -22,22 +24,27 @@ const drag = () => {
 
 <template>
   <div
-    :class="{
-      'hover:w-4': !unwatch,
-      'w-4': unwatch,
-    }"
-    class="cursor-pointer h-full w-2 bg-primary-200 isolate z-50 shadow transition-all"
-    draggable="true"
-    @click="updateProgress"
-    @mousedown="drag"
+    class="h-full justify-end w-4 flex z-50 cursor-pointer"
+    @mouseenter="hovering = true"
+    @mouseleave="hovering = false"
   >
     <div
       :class="{
-        'rounded-b-none': page.progress === 100,
+        'w-full': unwatch || hovering,
       }"
-      :style="{ height: `${page.progress}%` }"
-      class="h-full bg-primary rounded-b-full"
-    />
+      class="w-2 bg-primary-200 transition-all shadow"
+      draggable="true"
+      @click="updateProgress"
+      @mousedown="drag"
+    >
+      <div
+        :class="{
+          'rounded-b-none': page.progress === 100,
+        }"
+        :style="{ height: `${page.progress}%` }"
+        class="h-full bg-primary rounded-b-full"
+      />
+    </div>
   </div>
 </template>
 
