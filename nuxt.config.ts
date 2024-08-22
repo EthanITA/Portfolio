27a8f2a21f3a136163a8c3a68c7fa313f808b6e1
madong <viaimperiale@gmail.com>
2024-08-22 20:30:49 +0200

    build: add NuxtHub

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: ["@pinia/nuxt", "@nuxt/image"],
+  modules: ["@pinia/nuxt", "@nuxt/image", "@nuxthub/core"],
   css: ["~/assets/css/main.css"],
   postcss: {
     plugins: {
